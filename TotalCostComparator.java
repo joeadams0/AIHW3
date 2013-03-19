b784b7adf19bf05435893bba68c46625b825e9ac
Andrew Bruss <apb61@case.edu>
2013-03-18 22:45:57 -0400

    planner mostly completed

---
+++ TotalCostComparator.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+public class TotalCostComparator implements Comparator<StateNode> {
+
+	@Override
+	public int compare(StateNode x, StateNode y)
+	{
+	    // Assume neither string is null. Real code should
+        // probably be more robust
+        if (x.totalEstimatedCost() < y.totalEstimatedCost())
+        {
+            return -1;
+        }
+        if (x.totalEstimatedCost() > y.totalEstimatedCost())
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
